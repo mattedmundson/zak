3bc602dafe1171d3717cf0ad4988d6b6af2efce6
Matt Edmundson (Studio) <matt@aurioncompany.com>
2026-02-06 22:32:26 +0000

    Mobile Tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -229,7 +229,7 @@ export default async function Home() {
               <h3 className="text-2xl md:text-3xl font-bold text-gray-900 mb-10 text-center">How it works</h3>
               <div className="grid grid-cols-1 md:grid-cols-6 gap-8 md:gap-12 items-center">
                 {/* Image (col 2-3) */}
-                <div className="relative max-w-[60%] md:max-w-none md:col-start-2 md:col-span-2">
+                <div className="relative w-[90%] mx-auto md:w-auto md:col-start-2 md:col-span-2">
                   <div className="absolute -bottom-3 -right-3 w-full h-full bg-black" />
                   <div className="relative border border-black">
                     <Image
@@ -302,7 +302,7 @@ export default async function Home() {
                 </div>
 
                 {/* Image (col 4-5) */}
-                <div className="relative max-w-[60%] md:max-w-none md:col-span-2 order-1 md:order-2">
+                <div className="relative w-[90%] mx-auto md:w-auto md:col-span-2 order-1 md:order-2">
                   <div className="absolute -bottom-3 -right-3 w-full h-full bg-black" />
                   <div className="relative border border-black">
                     <Image
@@ -320,7 +320,7 @@ export default async function Home() {
               {/* Row 3 - Image left, Text right */}
               <div className="grid grid-cols-1 md:grid-cols-6 gap-8 md:gap-12 items-center mt-16 md:mt-20">
                 {/* Image (col 2-3) */}
-                <div className="relative max-w-[60%] md:max-w-none md:col-start-2 md:col-span-2">
+                <div className="relative w-[90%] mx-auto md:w-auto md:col-start-2 md:col-span-2">
                   <div className="absolute -bottom-3 -right-3 w-full h-full bg-black" />
                   <div className="relative border border-black">
                     <Image
@@ -363,7 +363,7 @@ export default async function Home() {
                 </div>
 
                 {/* Image (col 4-5) */}
-                <div className="relative max-w-[60%] md:max-w-none md:col-span-2 order-1 md:order-2">
+                <div className="relative w-[90%] mx-auto md:w-auto md:col-span-2 order-1 md:order-2">
                   <div className="absolute -bottom-3 -right-3 w-full h-full bg-black" />
                   <div className="relative border border-black">
                     <Image
